12f9ce2921513f57a2ddbb7695b7b6e46fe288d8
Javier Delgado Chacon <javierdelgadoch95@gmail.com>
2023-05-24 15:57:57 +0200

    basic gaming functionality completed

--- jugabilidad.js
+++ jugabilidad.js
@@ -11,8 +11,8 @@ window.onload = function () {
   
     const mensaje = document.getElementById('mensaje');
     let turno_jugador = true;
-    let puntos_jugador = 0; // Initialize points to 0
-    let puntos_bot = 0; // Initialize points to 0
+    let puntos_jugador = 0; // Inicializar puntos a 0
+    let puntos_bot = 0; // Inicializar puntos a 0
   
     function empezar() {
       let numero_random = Math.floor(Math.random() * 2);
@@ -26,6 +26,8 @@ window.onload = function () {
         mensaje.style.textAlign = "center";
         mensaje.style.borderRadius = "5px";
         turno_jugador = true;
+        jugar_jugador();
+        jugar_bot();
       } else if (numero_random === 1) {
         mensaje.textContent = "Empieza el bot";
         mensaje.style.color = "red";
@@ -33,72 +35,128 @@ window.onload = function () {
         mensaje.style.width = "255px";
         mensaje.style.textAlign = "center";
         mensaje.style.borderRadius = "5px";
-        mensaje.style.height = "35px";
+        mensaje.style.height = "55px";
         turno_jugador = false;
-        jugarBot();
+        jugar_bot();
+        mensaje.textContent = "El bot ha elegido. Ahora es tu turno";
+        jugar_jugador();
       }
-  
-      piedra_boton.addEventListener('click', function () {
-        if (turno_jugador) {
-          const opcion_jugador = piedra;
-          const opcion_maquina = jugarBot();
-          comparar(opcion_jugador, opcion_maquina);
-        }
-      });
-  
-      papel_boton.addEventListener('click', function () {
-        if (turno_jugador) {
-          const opcion_jugador = papel;
-          const opcion_maquina = jugarBot();
-          comparar(opcion_jugador, opcion_maquina);
-        }
-      });
-  
-      tijeras_boton.addEventListener('click', function () {
-        if (turno_jugador) {
-          const opcion_jugador = tijeras;
-          const opcion_maquina = jugarBot();
-          comparar(opcion_jugador, opcion_maquina);
-        }
-      });
     }
   
-    function jugarBot() {
+    function jugar_bot() {
       const opcion_maquina = Math.floor(Math.random() * 3);
   
       if (opcion_maquina === 0) {
-        mensaje.textContent = "El bot eligió Piedra";
         return piedra;
       } else if (opcion_maquina === 1) {
-        mensaje.textContent = "El bot eligió Papel";
         return papel;
       } else if (opcion_maquina === 2) {
-        mensaje.textContent = "El bot eligió Tijeras";
         return tijeras;
       }
+    }
+  
+    function jugar_jugador() {
+      piedra_boton.addEventListener('click', function () {
+          const opcion_jugador = piedra;
+          const opcion_maquina = jugar_bot();
+          comparar(opcion_jugador, opcion_maquina);
+        
+      });
+  
+      papel_boton.addEventListener('click', function () {
+          const opcion_jugador = papel;
+          const opcion_maquina = jugar_bot();
+          comparar(opcion_jugador, opcion_maquina);
+        
+      });
+  
+      tijeras_boton.addEventListener('click', function () {
+          const opcion_jugador = tijeras;
+          const opcion_maquina = jugar_bot();
+          comparar(opcion_jugador, opcion_maquina);
+      });
     }
   
     function comparar(opcion_jugador, opcion_maquina) {
       mensaje.textContent = "";
   
       if (opcion_jugador === opcion_maquina) {
-        puntos_bot++;
-        puntos_jugador++;
-        document.getElementById('puntos_jugador').textContent += '|';
-        document.getElementById('puntos_bot').textContent += '|';
+        console.log(puntos_jugador)
+        console.log(puntos_bot)
+        puntos_bot += 1;
+        puntos_jugador += 1;
+        console.log(puntos_jugador)
+        console.log(puntos_bot)
+        document.getElementById('puntos_jugador').textContent = puntos_jugador;
+        document.getElementById('puntos_bot').textContent = puntos_bot;
+        mensaje.style.color = "black";
+        mensaje.style.backgroundColor = "yellow";
+        mensaje.style.width = "255px";
+        mensaje.style.height = "55px";
+        mensaje.style.textAlign = "center";
+        mensaje.style.borderRadius = "5px";
         mensaje.textContent = "Empate. 1 punto para cada uno";
       } else if (
         (opcion_jugador === piedra && opcion_maquina === tijeras) ||
         (opcion_jugador === papel && opcion_maquina === piedra) ||
         (opcion_jugador === tijeras && opcion_maquina === papel)
       ) {
-        puntos_jugador++;
-        document.getElementById('puntos_jugador').textContent += '|';
+        console.log(puntos_jugador)
+        puntos_jugador += 1;
+        console.log(puntos_jugador)
+        document.getElementById('puntos_jugador').textContent = puntos_jugador;
+        mensaje.style.color = "green";
+        mensaje.style.backgroundColor = "lightgreen";
+        mensaje.style.width = "255px";
+        mensaje.style.height = "55px";
+        mensaje.style.textAlign = "center";
+        mensaje.style.borderRadius = "5px";
         mensaje.textContent = "Has ganado el punto";
-      } else {
-        puntos_bot++;
-        document.getElementById('puntos_bot').textContent += '|';
-        mensaje.textContent = "El bot ha ganado el punto";
+      } else if (
+        (opcion_jugador === tijeras && opcion_maquina === piedra) ||
+        (opcion_jugador === piedra && opcion_maquina === papel) ||
+        (opcion_jugador === papel && opcion_maquina === tijeras)
+      ) {
+        console.log(puntos_bot)
+        puntos_bot += 1;
+        console.log(puntos_bot)
+        document.getElementById('puntos_bot').textContent = puntos_bot;
+        mensaje.style.color = "red";
+        mensaje.style.backgroundColor = "lightred";
+        mensaje.style.width = "255px";
+        mensaje.style.height = "60px";
+        mensaje.style.textAlign = "center";
+        mensaje.style.paddingTop = "10px";
+
+        mensaje.style.borderRadius = "5px";
+        mensaje.textContent = "Punto para el bot";
+      }
+  
+      if (puntos_jugador >= 5 || puntos_bot >= 5) {
+        piedra_boton.removeEventListener('click', jugar_jugador);
+        papel_boton.removeEventListener('click', jugar_jugador);
+        tijeras_boton.removeEventListener('click', jugar_jugador);
+  
+        if (puntos_jugador > puntos_bot) {
+          mensaje.style.color = "white";
+          mensaje.style.backgroundColor = "green";
+          mensaje.style.width = "350px";
+          mensaje.style.height = "60px";
+          mensaje.style.textAlign = "center";
+          mensaje.style.paddingTop = "10px";
+          mensaje.style.borderRadius = "5px";
+          mensaje.innerHTML = 'HAS GANADO!! <span>&#x1F60A;</span>';
+          
+        } else if (puntos_bot > puntos_jugador) {
+          mensaje.style.color = "black";
+          mensaje.style.backgroundColor = "lightred";
+          mensaje.style.width = "350px";
+          mensaje.style.height = "55px";
+          mensaje.style.textAlign = "center";
+          mensaje.style.borderRadius = "5px";
+          mensaje.innerHTML = 'Te ha derrotado la máquina... <span>&#x1F916;</span>';
+
+        }
       }
     }
   
